aad7da40687672e1f272f25bbe5bc9385fa601cc
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-06-28 10:59:40 -0700

    Task 4

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for x in range(ord("a"), ord("z") + 1):
-    print("{}".format(chr(x)), end="")
+for r in range(ord("a"), ord("z") + 1):
+    print("{}".format(chr(r)), end="")
