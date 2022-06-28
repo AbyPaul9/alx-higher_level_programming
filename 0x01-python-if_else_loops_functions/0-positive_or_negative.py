3e1468f5569f3003e60aa191915fef319cf782d2
Akande Abayomi Paul <101406345+AbyPaul9@users.noreply.github.com>
2022-06-28 06:50:58 -0700

    Delete 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-  print("{number:d} is positive")
-elif number == 0:
-  print("{number:d} is zero")
-else:
-  print("{number:d} is negative")
