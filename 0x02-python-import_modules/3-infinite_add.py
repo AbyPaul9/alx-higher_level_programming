8abddaa6d61ba0d7ba1281018db387919f865382
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-07-06 05:44:01 -0700

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,9 @@ import sys
 
 
 def main(*argv):
-    l = len(sys.argv)
+    j = len(sys.argv)
     sum = 0
-    if l > 1:
+    if j > 1:
         for args in sys.argv:
             if args != sys.argv[0]:
                 sum = sum + int(args)
