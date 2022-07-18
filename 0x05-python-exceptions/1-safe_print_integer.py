541bca4df00670202192fd14e25dd0fe46f0befa
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-07-18 08:53:24 -0700

    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    prints an integer with "{:d}".format().
+    """
+
+
+def safe_print_integer(value):
+    try:
+        print('{:d}'.format(value))
+        return(True)
+    except:
+        return False
