855db81db690f99cc03ced4b70fd0febb04df5f8
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-07-19 08:20:08 -0700

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,53 +1,28 @@
 #!/usr/bin/python3
-""" Module Sqaure """
+""" Square class """
 
 
 class Square:
-    """ Square class defined by geometric shap
-        Attributes:
-            size (int): Size of square
+    """ empty class Square that defines a square
+    Attributes:
+        size: size of the square
     """
-    def __init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-        Returns:
-            None
-        """
-        self.__size = size
+    __size = 0
+
+    def __init__(self, prmSize=0):
+        self.size = prmSize
 
     def area(self):
-        """
-        set square square area
-        Return:
-            the current square area (int)
-        """
         return self.__size ** 2
 
     @property
     def size(self):
-        """
-        getter of size
-        Return:
-            Size of square
-        """
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """
-        Setter of size
-        Args:
-            size (int): size of a side of the square
-        Raises
-            TypeError: if size is not int
-            ValueError: size less than 0
-        Returns:
-            None
-        """
-        if type(value) is not int:
+    def size(self, prmSize=0):
+        if not isinstance(prmSize, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
-                raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        elif prmSize < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = prmSize
