e6e59fe34fea8aa5fbca842b3d9b9f56084b0e46
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-07-19 08:31:33 -0700

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square:
+    """ empty class Square that defines a square
+    Attributes:
+        size: size of the square
+    """
+    __size = 0
+
+    def __init__(self, prmSize=0):
+        self.size = prmSize
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, prmSize=0):
+        if not isinstance(prmSize, int):
+            raise TypeError("size must be an integer")
+        elif prmSize < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = prmSize
+
+    def my_print(self):
+        for y in range(self.size):
+            [print("#", end='') for x in range(self.size)]
+            print()
+        if self.size == 0:
+            print()
