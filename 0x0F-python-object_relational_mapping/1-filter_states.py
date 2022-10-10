c9d047e1df2704cfb3754cf080f4caf9fc3561fa
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-10-09 17:57:29 -0700

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
 """
-Module list state where name start with N
+    script that lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
 """
-import sys
+
+
 import MySQLdb
+import sys
 
 
 def main():
+    """
+        lists all states with a name starting with N (upper N)
+        from the database hbtn_0e_0_usa
+    """
     conn = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        charset="utf8"
-                            )
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cur.execute(query)
-    row = cur.fetchall()
-    for r in row:
-        if r[1][0] == 'N':
-            print(r)
+    req = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cur.execute(req)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
     conn.close()
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     main()
