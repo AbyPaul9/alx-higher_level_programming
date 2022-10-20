d946f2ae6373270e4224eecbe7582619a8b3f099
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-10-20 12:19:51 -0700

    Task 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #get statue code from http header  work just task(curl -sI "$1" | grep "HTTP/" |  cut -d " " -f 2)
-curl -s -o /dev/null --write-out "%{http_code}"
+curl -s -o /dev/null -w "%{http_code}" "$1"
