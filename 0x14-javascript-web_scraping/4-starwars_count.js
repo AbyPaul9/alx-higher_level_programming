ebbc6974d29d026cd92f3ae3b7eaffc4009d4a68
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-11-15 12:46:44 -0800

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
-request.get(url, function (err, response, body) {
-  let count = 0;
-  if (err) {
-    console.log(err);
-  }
-  const data = JSON.parse(body);
-  for (let i = 0; data.results[i] !== undefined; i++) {
-    if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      count++;
-    }
-  }
-  console.log(count);
-});
+
+let movieNb = 0;
+
+if (process.argv.length === 3) {
+  request(process.argv[2], { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.results.forEach(movie => {
+      movie.characters.forEach(character => {
+        if (character.indexOf('people/18') >= 0) {
+          movieNb = movieNb + 1;
+        }
+      });
+    });
+    console.log(movieNb);
+  });
+}
